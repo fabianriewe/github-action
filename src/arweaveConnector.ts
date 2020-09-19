3620a8c409f8bbd447d1d1be553d3aae741302bc
fabianriewe <f.riewe@riewe-software.de>
2020-09-19 22:11:35 +0200

    more logging

--- src/arweaveConnector.ts
+++ src/arweaveConnector.ts
@@ -14,6 +14,7 @@ const arweave = Arweave.init(
 
 
 export default async function storeOnArweave(newSignedCommitters: CommittersDetails[]) {
+    core.info("Inside storeOnArweave")
     const key: any = core.getInput('arweave-keyfile') //TODO change this top shared
 
     let tx_ids: string[] = [];
